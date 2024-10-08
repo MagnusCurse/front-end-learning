11f280f9d2813c8d567c558bdf24743c6ebb7f9f
liu-jinxii <arny1371621210@163.com>
2024-10-08 16:42:42 +0800

    some stysheet error(not fixed still)

--- product-and-cart/src/main.js
+++ product-and-cart/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-
+// import all styles by importing style.scss
+import './assets/styles/style.scss'
+import 'normalize.css'
 
 
 createApp(App).use(router).mount('#app')
