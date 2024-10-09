aaf045a566955f3822338723a6e952b94d48a34b
liu-jinxii <arny1371621210@163.com>
2024-10-09 20:11:02 +0800

    fix the bug can't find the normalize

--- product-and-cart/src/main.js
+++ product-and-cart/src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 
 // import all styles by importing style.scss
 import './assets/styles/style.scss'
-import 'normalize.css'
 
 
 createApp(App).use(router).mount('#app')
